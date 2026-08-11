961dbdafe41ac2aeaf76dab7dba8e8a03ec76e4a
jwn7 <ksyzing@naver.com>
2026-08-11 11:31:53 +0900

    chore: initialize frontend and backend projects

---
+++ backend/src/test/java/com/kiobridge/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiobridge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
